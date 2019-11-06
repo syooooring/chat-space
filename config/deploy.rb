8beea074a496d5a8cb5412712015b79d205585ff
syooooring <56821781+syooooring@users.noreply.github.com>
2019-11-06 18:06:38 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'ChatSpace'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:syooooring/chat-space.git'
